34e906bd65580dd43a0eb9c5858bbfb2275e5bfb
sonu2k1 <sonuks654321@gmail.com>
2026-04-16 20:54:11 +0530

    Replace inline SVG with logo.svg in Navbar and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,10 +28,7 @@ const Footer = () => {
             {/* Brand */}
             <div className="footer__brand">
               <div className="footer__logo">
-                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="8" y="4" width="20" height="20" rx="10" stroke="currentColor" strokeWidth="2.5" fill="none"/>
-                  <path d="M8 14V36" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round"/>
-                </svg>
+                <img src="/logo.svg" alt="Priya Interior Studio Logo" className="footer__logo-img" />
                 <div>
                   <span className="footer__logo-title">Priya</span>
                   <span className="footer__logo-subtitle">Interior Studio</span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,10 +36,7 @@ const Navbar = () => {
       <div className="navbar__container">
         <a href="/" className="navbar__logo" id="logo-link">
           <div className="navbar__logo-icon">
-            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="8" y="4" width="20" height="20" rx="10" stroke="currentColor" strokeWidth="2.5" fill="none"/>
-              <path d="M8 14V36" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round"/>
-            </svg>
+            <img src="/logo.svg" alt="Priya Interior Studio Logo" className="navbar__logo-img" />
           </div>
           <div className="navbar__logo-text">
             <span className="navbar__logo-title">Priya</span>
